b0ed977ab469a077afedff4a9adefddc2090fa36
kindleschneider27-star <kindle.schneider27@gmail.com>
2026-09-12 17:24:43 -0700

    test commission employee
    i created methods to test commission employee methods increasesales, holidaybonus and annualraise. i changes increasesales to only accept positive numbers

--- src/test/java/EmployeeObjects/CommissionEmployeeTest.java
+++ src/test/java/EmployeeObjects/CommissionEmployeeTest.java
@@ -1,7 +1,29 @@
 package EmployeeObjects;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommissionEmployeeTest {
+    CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
+
+    @Test
+    void testIncreaseSalesPositive() {
+        emp.increaseSales(10);
+        emp.increaseSales(-5);
+        assertEquals(10, emp.getSales());
+    }
+
+    @Test
+    void testHolidayBonus() {
+        assertEquals(0, emp.holidayBonus());
+
+    }
+
+    @Test
+    void testAnnualRaise() {
+        emp.annualRaise();
+        emp.annualRaise();
+        assertEquals(.0305, emp.getRate());
+    }
 
 }
